62a3a2dbba3b4a3b19ff90f0401cd5128d7d9e4f
Avish PT <Avish@gmail.com>
2024-02-22 22:47:31 +0530

    adding new file

---
+++ ShellScipts/JavaInstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# A script to download and install Java on Unix
+
+sudo yum install java -y
+sudo yum install java -y
---
+++ ShellScipts/JavaInstall_SH.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# A script to download and install Java on
+
+yum install java -y
